4e909769cbdd277e873d6e3d7fb3a366febdf62e
Brandon <blu006@ucr.edu>
2023-07-20 18:28:06 -0700

    Removed interface to internal Petitmodbus variables from the port file.

--- inc/PetitModbusPort.h
+++ inc/PetitModbusPort.h
@@ -14,19 +14,6 @@
 #define TRUE (1)
 #endif
 
-extern unsigned short PetitModbusTimerValue;
-
-extern unsigned int PetitRxCounter;
-extern unsigned int PetitRxRemaining;
-extern unsigned char* Petit_Rx_Ptr;
-extern unsigned char PetitRxTxBuffer[];
-extern unsigned char PetitExpectedReceiveCount;
-
-extern unsigned int Petit_Tx_Buf_Size;
-extern unsigned char* Petit_Tx_Ptr;
-
-extern PETIT_RXTX_STATE Petit_RxTx_State;
-
 // functions for porting
 extern void PetitPortUartBegin(unsigned char tx);
 extern void PetitPortTimerStart(void);
